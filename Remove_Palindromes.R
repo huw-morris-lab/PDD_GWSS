9649251bac82970c9e2201ddff7df848dcd99291
raquelreal <64485023+raquelreal@users.noreply.github.com>
2022-04-08 22:02:55 +0100

    Create Remove_Palindromes.R

---
+++ Remove_Palindromes.R
@@ -0,0 +1,22 @@
+
+#!/usr/bin/env Rscript
+args = commandArgs(trailingOnly=TRUE)
+
+library(data.table)
+library(dplyr) 
+
+# Removing palindromes (change path for each file)
+bim = read.table(args[1], header=F)
+
+#Get indices of A/T and G/C SNPs
+w = which((bim$V5=="A" & bim$V6=="T") |
+(bim$V5=="T" & bim$V6=="A") |
+(bim$V5=="C" & bim$V6=="G") |
+(bim$V5=="G" & bim$V6=="C"))
+
+#Extract A/T and G/C SNPs
+at.cg.snps = bim[w,]
+
+#Save
+write.table(at.cg.snps$V2,"at-cg-snps.txt", row.names = F, col.names = F, quote = F)
+q("no")
