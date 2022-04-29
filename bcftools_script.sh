dc77a709e7e4093483af236a78dbb0fc4a089260
raquelreal <64485023+raquelreal@users.noreply.github.com>
2022-04-29 15:27:22 +0100

    Update bcftools_script.sh

--- bcftools_script.sh
+++ bcftools_script.sh
@@ -1,5 +1,5 @@
 COHORT=cohort_name
-DIR=path_to_imputed_files
+DIR=path/to/imputed/files
 bcftools concat $DIR/chr1.dose.vcf.gz $DIR2/chr2.dose.vcf.gz $DIR/chr3.dose.vcf.gz $DIR/chr4.dose.vcf.gz $DIR/chr5.dose.vcf.gz $DIR/chr6.dose.vcf.gz $DIR/chr7.dose.vcf.gz $DIR/chr8.dose.vcf.gz $DIR/chr9.dose.vcf.gz $DIR/chr10.dose.vcf.gz $DIR/chr11.dose.vcf.gz $DIR/chr12.dose.vcf.gz $DIR/chr13.dose.vcf.gz $DIR/chr14.dose.vcf.gz $DIR/chr15.dose.vcf.gz $DIR/chr16.dose.vcf.gz $DIR/chr17.dose.vcf.gz $DIR/chr18.dose.vcf.gz $DIR/chr19.dose.vcf.gz $DIR/chr20.dose.vcf.gz $DIR/chr21.dose.vcf.gz $DIR/chr22.dose.vcf.gz -Ou | 
 bcftools view -Ou -i 'R2>0.3' |
 bcftools norm -Ou -m -any |
